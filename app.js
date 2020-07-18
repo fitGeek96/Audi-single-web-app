aa2af33316696b26db569813a42c3468f66bca87
fitgeek96 <fitgeek96@gmail.com>
2020-07-18 10:19:29 +0100

    add wheels content

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const contentDetailEl = document.querySelector(".content__details");
 const dotsColorEl = document.querySelector(".dots");
 const hamburgerMenuEl = document.querySelector(".hamburger");
 const navEl = document.querySelector(".nav");
+const homeBtnEl = document.querySelector(".menu__items").firstElementChild;
+const wheelsBtnEl = homeBtnEl.nextElementSibling;
+const monitorsBtnEl = wheelsBtnEl.nextElementSibling;
+const categoriesBtnEl = monitorsBtnEl.nextElementSibling;
+
+console.log(categoriesBtnEl);
+
 
 
 
@@ -45,5 +52,43 @@ function openMenu() {
     });
 }
 
+function home() {
+    homeBtnEl.addEventListener("click", function () {
+        contentEl.innerHTML = `<div class="content__title">
+        <h1>Audi RS7</h1>
+      </div>
+      <div class="content__details">
+        <img src="images/rs7__white.png" alt="audi rs7 image" class="slide-in-blurred-left">
+      </div>
+      <div class="dots">
+        <div class="dot white"></div>
+        <div class="dot grey"></div>
+        <div class="dot red"></div>
+      </div>`;
+    });
+}
+
+function wheels(){
+    wheelsBtnEl.addEventListener("click", function(){
+        contentEl.innerHTML = ` <div class="content__title">
+        <h1>Audi RS7 Wheels</h1>
+      </div>
+      <div class="content__details wheel__container">
+        <div class="wheels">
+          <img src="images/wheel__4.png" alt="audi rs7 wheels image" class="scale-in-ver-bottom-1">
+          <img src="images/wheel__2.png" alt="audi rs7 wheels image" class="scale-in-ver-bottom-2">
+          <img src="images/wheel__3.png" alt="audi rs7 wheels image" class="scale-in-ver-bottom-3">
+        </div>
+        <div class="wheels">
+          <img src="images/wheel__1.png" alt="audi rs7 wheels image" class="scale-in-ver-bottom-4">
+          <img src="images/wheel__5.png" alt="audi rs7 wheels image" class="scale-in-ver-bottom-5">
+          <img src="images/wheel__6.png" alt="audi rs7 wheels image" class="scale-in-ver-bottom-6">
+        </div>
+      </div>`;
+    });
+}
+
 changeCarColor();
 openMenu();
+home();
+wheels();
